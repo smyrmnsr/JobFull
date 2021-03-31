387a59af93a14f4ff582776fd97b62ae48c1b17b
Moraru Andrei <53907172+Andrei5Moraru@users.noreply.github.com>
2021-03-31 20:53:48 +0300

    base url fix

--- src/components/jobAplicant/DisplayAllApplicants.js
+++ src/components/jobAplicant/DisplayAllApplicants.js
@@ -1,5 +1,5 @@
 import React, {  useState, useEffect } from 'react'
-import BASE_URL from "../BASE_URL";
+import BASE_URL from "../../BASE_URL";
 import axios from 'axios';
 
 const DisplayAllApplicants = (props)=> {
